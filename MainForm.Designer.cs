4f9c62f208853f2854b05e8db3c1fced787d2e31
30jannik06 <mcyolo17@gmail.com>
2025-07-09 13:14:09 +0200

    Update

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,10 +28,18 @@ partial class MainForm
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+        SuspendLayout();
+        // 
+        // MainForm
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Icon = (Icon)resources.GetObject("$this.Icon");
+        Name = "MainForm";
+        Text = "Form1";
+        ResumeLayout(false);
     }
 
     #endregion
